fcca5a067e3ea119737a3c368c06bca5830910f5
TheGuler0x <95317318+theguler0x@users.noreply.github.com>
2023-11-02 21:31:16 +0300

    Update Mass ACL Reporter.ps1

--- Mass ACL Reporter.ps1
+++ Mass ACL Reporter.ps1
@@ -1,39 +1,54 @@
-﻿# Active Directory module import
+# Active Directory module import
 Import-Module ActiveDirectory
 
 # DC name and root OU
-# [DistinguishedName]
 $domainController = "DC=guler,DC=com"
 $rootOU = Get-ADOrganizationalUnit -Filter * -SearchBase $domainController
 
 # HTML rapor dosyası oluştur
 $htmlReportPath = "C:\AD_ACL_Rapor.html"
-$htmlReportContent = "<html><head><title>Active Directory ACL Rapor</title></head><body>"
+$htmlReportContent = "<html><head><title>Active Directory ACL-ACE Report</title></head><body>"
 
-# Başlık ekleyin
-$htmlReportContent += "<h1>#All Domain OU Mass ACL Reporter:</h1>"
-$htmlReportContent += "<h1>#| TheGuler0x |</h1>"
 
-# All OU Foreach
+# Başlık
+$htmlReportContent += "<h2># All Domain OU Mass ACL-ACE Reporter:📜</h2>"
+# Ana domain ACL bilgilerini rapora ekleyin
+$htmlReportContent += "<h2># | TheGuler0x | 🐝</h2>"
+
+# Ana domain ACL bilgilerini al
+$domainACL = dsacls $domainController
+
+# Generated tarihi
+$htmlReportContent += "<p>Generated: $reportDateTime</p>"
+$htmlReportContent += "<p>Bilgisayar Adı: $computerName</p>"
+$htmlReportContent += "<p>Etki Alanı Adı: $domainName</p>"
+
+
+# Ana domain name
+$htmlReportContent += "<h2><font color='green'>#Main Domain: $domainController</font></h2>"
+
+$domainACL -split "`r`n" | ForEach-Object {
+    $htmlReportContent += "<pre>$_</pre>"
+}
+
+# Tüm OU'ları işleyin
 foreach ($ou in $rootOU) {
     $ouDN = $ou.DistinguishedName
     $ouName = $ou.Name
     $ouACL = dsacls "$ouDN"
 
-    # OU başlığı
-    $htmlReportContent += "<h2>OU: $ouName</h2>"
+    # OU başlığı (sarı renkle vurgulandı)
+    $htmlReportContent += "<h2><font color='green'>#OU: $ouName</font></h2>"
 
     # dsacls çıktısını satır satır ekleyin
     $ouACL -split "`r`n" | ForEach-Object {
         $htmlReportContent += "<pre>$_</pre>"
     }
 
-    # Her OU sonrasında bir boş satır ekleyin
-    $htmlReportContent += "<br>"
+    # Özel işaretleme ekleyin
+    $htmlReportContent += "<p style='color: red; font-weight: bold;'>📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃📃</p>"
 }
 
 # HTML raporunu kapat
 $htmlReportContent += "</body></html>"
 $htmlReportContent | Out-File -FilePath $htmlReportPath
-
-Write-Host "HTML rapor oluşturuldu: $htmlReportPath"
